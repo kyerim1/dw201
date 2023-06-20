c748b13395cc5ad5f770ff053cf7fe4e5fd30e6a
kyerim1 <nilzero25@gmail.com>
2023-06-20 15:26:05 +0900

    0620

---
+++ study0620/static/js/array5.js
@@ -0,0 +1,14 @@
+
+
+window.onload=function(){
+   let pic = document.getElementsByClassName("picture");
+   for( var i=0; i<pic.length; i++){
+        pic[i].addEventListener("click",same_search);
+        pic[i].innerHTML = Math.floor(Math.random()*10)+1;
+    }
+
+}
+
+function  same_search(){
+    
+}
