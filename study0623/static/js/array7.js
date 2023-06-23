04fb6b84a516ea07303d09300700d28a12e3206c
kyerim1 <nilzero25@gmail.com>
2023-06-23 11:17:49 +0900

    23일 로또

--- study0623/static/js/array7.js
+++ study0623/static/js/array7.js
@@ -1,9 +1,25 @@
-
+/*
+    현재 태그(엘리먼트)의 다음 태그를 가져오는방법
+    - .nextSibling   (형제태그를 의미한다.)
+    태그(엘리먼트) 객체에 클래스를 추가하는 방법
+    -  element.classList.add('클래스이름');
+    클래스 여러개 추가
+    - element.classList.add('클래스이름','클래스이름');
+    클래스 이름 변경
+    - element.classList.replace('변경전이름','변경후이름');
+    클래스 삭제
+    -  element.classList.toggle('삭제할 클래스 이름');
+    조건에 따라 클래스 삭제
+    - element.classList.toggle('삭제클래스이름',조건식);
+    태그(element) 객체에 클래스가 있는지 확인방법
+    -  element.classList.contains('클래스이름');
+    -  해당 클래스이름이 있다면 true 없다면 false
+*/
 window.onload=function(){
 // 화면이 전부 로딩 되면 시작하는 함수
     var icon = document.getElementsByClassName("strapIcon");    
     icon[0].addEventListener("click",function(){
-        var list = document.getElementsByClassName("menu_list");
-        list[0].style.display="block";
+        var list = this.nextSibling;
+        list.classList.add("list_active");
     });
 }
