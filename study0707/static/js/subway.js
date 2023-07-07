dd72566bfbb46547cc5402cc09cee2b91b607a82
kyerim1 <nilzero25@gmail.com>
2023-07-07 11:39:10 +0900

    지하철

--- study0707/static/js/subway.js
+++ study0707/static/js/subway.js
@@ -17,15 +17,31 @@ function map_draw(){ //지도 그리기 위한 함수
     var out="";
     for(var line=0; line<4; line++){
         var t=line*10;
-        if( line%2!=0)  t+=9;
-        whlie( ){ // 1줄 마지막인덱스 9, 2줄마지막인덱스 10, 3줄 마지막인덱스 29, 4줄 마지막인덱스 30
-            out += "<div class='station'>";
-            out += "<div class='train'><i class='fa-solid fa-train'></i></div>";
-            out += "<div class='mark'><div class='rail'></div>"+
-            "<span class='stop'><i class='fa-regular fa-square'></i></span>"+
-            "</div>";
-            out += "<div class='name'>" +st_name[t]+ "</div></div>";
+        if( line%2!=0){  t+=9;
+            while( t >= line*10) //  1줄 마지막인덱스 9, 2줄마지막인덱스 10, 3줄 마지막인덱스 29, 4줄 마지막인덱스 30
+                out += make(t--);
+        }else{
+            while(t <= line*10+9)
+                out += make(t++);
         }
+            
     }
     map.innerHTML=out;
+}
+
+function make(t){
+    var w95="";
+    if(t==9 || t==10 || t==19|| t==20 || t==29|| t==30)
+        w95="w95";
+
+    var out="";
+    out += "<div class='station'>";
+    out += "<div class='train'><i class='fa-solid fa-train'></i></div>";
+    out += "<div class='mark'><div class='rail "+w95+"'></div>"+
+            "<span class='stop'><i class='fa-regular fa-square'></i></span>";
+    if(t%10==9 && t!=39)
+        out+="<div class='rad "+(t==19?'right':'left')+"'></div>";
+    out += "</div>";
+    out += "<div class='name'>" +st_name[t]+ "</div></div>";
+    return out;
 }
