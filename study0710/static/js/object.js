59cde810ff3ec87af0cd0262ed165b75de5e7b2f
kyerim1 <nilzero25@gmail.com>
2023-07-10 11:33:08 +0900

    winopen

--- study0710/static/js/object.js
+++ study0710/static/js/object.js
@@ -15,12 +15,25 @@ window.onload = function(){
 function exit(){
     window.close();
 }
+let child="";
 function winopen(){
-    window.open("https://www.naver.com","_blank","");
+    child = window.open("./child.html","_blank","width=500,height=400");
 //window.open("주소","새창의이름또는 타겟","옵션");
 
 // open함수의 두번째 인자로 
 // _blank 또는 아무것도 넣지않으면
 // 현재 브라우저에 새탭으로 열기하려면 옵션부분에 너비,높이 지정안하다.
 // 너비와 높이를 지정하면 새 브라우저로 열기가된다.
+}
+function child_getName(){
+    //자식창의 id가 name인 input값 가져오기
+    var name = child.document.getElementById("name").value;
+    //부모창의 div에 출력
+    document.getElementById("name").innerHTML=name;
+}
+function child_write(){
+    child.document.getElementById("message").innerText="머리카락 휘날리며 와";
+}
+function child_close(){
+    child.window.close();
 }
