8ac833fa0c2d1395af7e765edaa6cc36b47b974e
kyerim1 <nilzero25@gmail.com>
2023-07-26 15:20:49 +0900

    0726

--- 726study/static/js/romantic.js
+++ 726study/static/js/romantic.js
@@ -11,6 +11,17 @@ function date_list(place, money, period, satisfaction){
     this.period=period;
     this.satisfaction=satisfaction;
 }
+date_list.prototype.view=function(){
+    return "<li class='Dlist'><span class='place_vw'>"+this.place+"</span>"+
+    "<span class='money_vw'>"+this.money+"</span>"+
+    "<span class='period_vw'>"+this.period+"</span>"+
+    "<span class='stf_vw'>"+this.satisfaction+"</span>"+
+    "<b class='update'><i class='bi bi-wrench'></i></b>"+
+    "<b class='del'><i class='bi bi-trash3'></i></b> </li>";
+}
+
+
+
 const list = new Array();  // date_list 객체를 저장할 배열 객체 생성
 function save(){
     var place=document.querySelector("#place");
@@ -20,11 +31,26 @@ function save(){
 //input 태그  객체로 가져오기
     if ( value_check( [place,money,period] ) ) return;
 
-    list.push(  );
-    
-    alert(list[0].place);
+    list.push( new date_list(place.value, money.value, period.value, stf.options[stf.selectedIndex].value ) );
 
+    //input 초기화
+    init( [place,money,period,stf] );
+
+    //화면 출력
+    screen_show();
+
+    console.log(list);
 }
+
+function screen_show(){
+    var ul = document.querySelector("#list");
+    //for(var i=0; i<list.length; i++)
+    for(var i in list){
+        list[i]
+    }
+}
+
+
 function value_check(input){ // input  값의 유효 확인
     const msg=["데이트 장소를 입력하세요","데이트비용을 입력하세요",
 "연애가 처음인가요"]; // input에 미입력시 알림 멘트
@@ -39,7 +65,12 @@ function value_check(input){ // input  값의 유효 확인
     return false;   
 }
 function init(input){ // input을 초기화
+    for(var i=0; i<input.length-1; i++){
+        input[i].value='';
+    } //input 태그의 value 비우기
 
+    input[3].options[4].selected=true;  // 만족도 기본값 5 설정
+    input[0].focus(); // 데이트장소 input에 마우스커서 위치하기
 }
 
 
