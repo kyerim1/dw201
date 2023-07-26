611cd9c6552d3f622d6a07829b855e5fb4a37619
kyerim1 <nilzero25@gmail.com>
2023-07-26 14:30:02 +0900

    0726

---
+++ 726study/static/js/romantic.js
@@ -0,0 +1,49 @@
+
+window.addEventListener("keypress",function(e){ //엔터 누르면 save함수 동작
+    if(e.keyCode==13){
+        save();
+    }
+});
+
+function date_list(place, money, period, satisfaction){
+    this.place=place;
+    this.money=money;
+    this.period=period;
+    this.satisfaction=satisfaction;
+}
+const list = new Array();  // date_list 객체를 저장할 배열 객체 생성
+function save(){
+    var place=document.querySelector("#place");
+    var money=document.getElementById("money");
+    var period = document.querySelector("#period");
+    var stf = document.querySelector("#satisfaction");
+//input 태그  객체로 가져오기
+    if ( value_check( [place,money,period] ) ) return;
+
+    list.push(  );
+    
+    alert(list[0].place);
+
+}
+function value_check(input){ // input  값의 유효 확인
+    const msg=["데이트 장소를 입력하세요","데이트비용을 입력하세요",
+"연애가 처음인가요"]; // input에 미입력시 알림 멘트
+
+    for(var i=0; i<input.length; i++){  
+        if( input[i].value==''){ // 장소, 비용, 기간 순으로 미입력 여부 확인
+            alert(msg[i]);
+            input[i].focus(); // 미입력한 곳에 커서 위치하기
+            return true;
+        }
+    }
+    return false;   
+}
+function init(input){ // input을 초기화
+
+}
+
+
+window.onload=function(){
+
+};
+
