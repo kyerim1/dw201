c87d0859a47360e88fafed23320fca0ab5f5c7fa
kyerim1 <nilzero25@gmail.com>
2023-07-26 16:22:11 +0900

    0726

--- 726study/static/js/romantic.js
+++ 726study/static/js/romantic.js
@@ -12,16 +12,21 @@ function date_list(place, money, period, satisfaction){
     this.satisfaction=satisfaction;
 }
 date_list.prototype.view=function(){
-    return "<li class='Dlist'><span class='place_vw'>"+this.place+"</span>"+
+    return "<span class='place_vw'>"+this.place+"</span>"+
     "<span class='money_vw'>"+this.money+"</span>"+
     "<span class='period_vw'>"+this.period+"</span>"+
     "<span class='stf_vw'>"+this.satisfaction+"</span>"+
     "<b class='update'><i class='bi bi-wrench'></i></b>"+
-    "<b class='del'><i class='bi bi-trash3'></i></b> </li>";
+    "<b class='del'><i class='bi bi-trash3'></i></b>";
 }
 
 
 const list = new Array();  // date_list 객체를 저장할 배열 객체 생성
+
+let update_tag='';  
+let del_tag='';
+
+
 function save(){
     var place=document.querySelector("#place");
     var money=document.getElementById("money");
@@ -38,7 +43,7 @@ function save(){
     //화면 출력
     screen_show();
 
-    console.log(list);
+
 }
 
 function screen_show(){
@@ -46,9 +51,28 @@ function screen_show(){
     //for(var i=0; i<list.length; i++)
     var out="";
     for(var i in list){
-        out += list[i].view();
+        out += "<li class='Dlist' data-idx='"+i+"'>"+list[i].view()+" </li>";
     }
     ul.innerHTML= out;
+
+    update_tag = document.querySelectorAll(".update");
+    del_tag =document.querySelectorAll(".del");
+
+    for(var i=0; i<update_tag.length; i++)
+        update_tag[i].addEventListener("click",update_list);
+    for(var i=0; i<del_tag.length; i++){
+        del_tag[i].addEventListener("click",delete_list);
+    }
+}
+function update_list(){
+
+}
+function delete_list(){
+    var del_idx = this.parentNode.dataset.idx;  // 현재클릭한 태그는 b태그이고 b태그의 부모인 li태그 찾기
+    //  부모태그는  parentNode 라고 하면된다.  부모태그의 dataset 값 가져오기
+    //list.splice(2,2);  2번인덱스 부터 2개 추출 하기
+    list.splice(del_idx,1);
+    screen_show();
 }
 
 
