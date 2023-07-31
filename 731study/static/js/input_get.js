04eda458b3f377d933adb2e8dcf76c3aedc4d277
kyerim1 <nilzero25@gmail.com>
2023-07-31 16:19:41 +0900

    0731

--- 731study/static/js/input_get.js
+++ 731study/static/js/input_get.js
@@ -5,6 +5,13 @@
 // prepend - 선택한 태그의 처음에 추가
 // before - 선택한 태그의 앞쪽에 추가
 // after - 선택한 태그의 뒤쪽에 추가
+
+// remove - 선택한 태그를 삭제
+// empty - 선택한 태그안에 삭제
+
+//removeClass - 선택한 태그에 지정한 클래스 삭제
+//addClass - 선택한 태그에 지정한 클래스 추가
+
 let ri=1;
 
 $(function(){
@@ -19,6 +26,7 @@ $(function(){
         $("#gallery").addClass("grid-3n");
         $("#gallery").removeClass("slide-active");
         $("#gallery").addClass("slide-off");
+        $("#gallery").append($(".img_box"));
         //$("#gallery").css("grid-template-columns","repeat(3,1fr)");
     });
    
@@ -27,11 +35,17 @@ $(function(){
         $("#gallery").addClass("grid-4n");
         $("#gallery").removeClass("slide-active");
         $("#gallery").addClass("slide-off");
+        $("#gallery").append($(".img_box"));
     });
 
 // 슬라이드
     $("#slide").click(function(){
+        $("#gallery").removeClass("grid-4n");
+        $("#gallery").removeClass("grid-3n");
         $("#gallery").removeClass("slide-off");
         $("#gallery").addClass("slide-active");
+        $("#slide_box").append($(".img_box"));
+        var len = $(".img_box").length;
+        $("#slide_box").css("width",len*1200+"px");
     });
 });
