b99f28be2b8d7615ea9c22f238793a1b0986b20c
kyerim1 <nilzero25@gmail.com>
2023-08-08 10:36:51 +0900

    0808

--- 8월/0807study/static/js/health.js
+++ 8월/0807study/static/js/health.js
@@ -34,20 +34,36 @@ $(function(){
     });
 
 
-    $(".centerName").click(function(){
+    $(".sort_obj").click(function(){
         $(this).toggleClass("asc");
         $(this).toggleClass("desc");
+        var name = $(this).data("name");
+        const sort_type= { centerName:"건강증진센터명" ,
+addr:"소재지도로명주소" , nurseCount:"간호사수", doctorCount:"의사수"};
+        var has = -1;
+        if( $(this).hasClass("asc") ) has=1;
+        
+        $.getJSON("./전국건강증진센터표준데이터.json",function(data){
+            const data_list = data.records;
+           
+            data_list.sort(function(a,b){
+                if( a[sort_type[name]] > b[sort_type[name]]) return 1*has;
+                if( a[sort_type[name]] < b[sort_type[name]]) return -1*has;
+                if(a[sort_type[name]]===b[sort_type[name]]) return 0;
+            });
+
+            $("#section").empty();
+           $.each(data_list, function(i, item){
+                $("#section").append(
+    "<div class='item_short'><div class='item_image'>"+
+    "<img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
+    "<div class='item_detail_box'><ul class='item_detail'>"+
+    "<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
+    "<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
+    "<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>"
+                );
+           });
+        });
     });
-    $(".addr").click(function(){
-        $(this).toggleClass("asc");
-        $(this).toggleClass("desc");
-    });
-    $(".nurseCount").click(function(){
-        $(this).toggleClass("asc");
-        $(this).toggleClass("desc");
-    })
-    $(".doctorCount").click(function(){
-        $(this).toggleClass("asc");
-        $(this).toggleClass("desc");
-    })
+   
 });
