4970bda24c043e26df627c15a599b54694bad3c2
kyerim1 <nilzero25@gmail.com>
2023-08-08 14:35:28 +0900

    0808

--- 8월/0807study/static/js/health.js
+++ 8월/0807study/static/js/health.js
@@ -1,24 +1,39 @@
-$(function(){
+//  스크립트에서 배열을 생성할때는  const를 많이 사용한다.  
+//  배열의 주소는 변하지 않고 값만 변동이 있기때문에  배열의 주소변조를 막기위해 const를 사용한다.
+/*
+    자바스크립트 비동기 -  aysnc  , await, promose 
+    자바스크립트에서 비동기 처리 3가지 방법
+     1. 콜백함수  -  무한 콜백, callback hell 
+     2. promise 
+     3. aysnc/await
+
+*/
+let data_list= new Object(); // json 데이터 저장할 전역변수
+// $.getJSON("./전국건강증진센터표준데이터.json",function(data){
+//     data_list =data.records;
+//  });
+ 
+async function getData(){
+        // var d="";
+        // $.getJSON("./전국건강증진센터표준데이터.json",function(data){
+        //     d =data.records;
+        // });
+        // return d;
+        var data = await fetch("./전국건강증진센터표준데이터.json").then(function(res){ return res.json();}).then(function(r){return r;});
+        console.log(data.records);
+        return data.records;
+}
+
+$(async function(){
     $(".filterMore").click(function(){
         $(this).toggleClass("moreDown");
         $(this).toggleClass("moreUp");
         $(".filterDetail").slideToggle();
     });
+   data_list = await getData();
+   view(data_list);
 
-    $.getJSON("./전국건강증진센터표준데이터.json",function(data){
-        const data_list = data.records;
-
-       $.each(data_list, function(i, item){
-            $("#section").append(
-"<div class='item_short'><div class='item_image'>"+
-"<img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
-"<div class='item_detail_box'><ul class='item_detail'>"+
-"<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
-"<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
-"<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>"
-            );
-       });
-    });
+    
 
 // 전체 텍스트에 대한 검색이 아니라
 //  소재지주소와 업무내용에 한해서만  검색이 가능하게 변경하시오
@@ -52,18 +67,24 @@ addr:"소재지도로명주소" , nurseCount:"간호사수", doctorCount:"의사
                 if(a[sort_type[name]]===b[sort_type[name]]) return 0;
             });
 
-            $("#section").empty();
-           $.each(data_list, function(i, item){
-                $("#section").append(
-    "<div class='item_short'><div class='item_image'>"+
-    "<img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
-    "<div class='item_detail_box'><ul class='item_detail'>"+
-    "<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
-    "<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
-    "<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>"
-                );
-           });
+           view(data_list);
+           
         });
     });
    
+
 });
+
+function view(data_list){
+    $("#section").empty();
+    $.each(data_list, function(i, item){
+        $("#section").append(
+"<div class='item_short'><div class='item_image'>"+
+"<img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
+"<div class='item_detail_box'><ul class='item_detail'>"+
+"<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
+"<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
+"<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>"
+        );
+   });
+}
