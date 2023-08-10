4d3a715c4610b8a06c51864df89893769d5839bf
kyerim1 <nilzero25@gmail.com>
2023-08-10 11:45:48 +0900

    0810

--- 8월/0810study/static/js/chart1.js
+++ 8월/0810study/static/js/chart1.js
@@ -23,9 +23,33 @@ $(function(){
         $("#name").focus();
     });
 });
-
+let oldx=oldy=0;
 function draw(name,tall){
 
+    ctx.fillStyle="#000";
+    ctx.font="20px Arial";
+    ctx.fillText(name,50+100*cnt,680);
+
+    ctx.beginPath();
+    ctx.arc(80+100*cnt,600-tall,3,0,2*Math.PI);
+    ctx.fillStyle="red";
+    ctx.fill();
+
+    if(oldx==0 && oldy==0){
+        oldx=80+100*cnt;
+        oldy=600-tall;
+    }else{
+        ctx.moveTo(oldx,oldy);
+        ctx.lineTo(80+100*cnt,600-tall);
+        ctx.stroke();
+
+        oldx=80+100*cnt;
+        oldy=600-tall;
+    }
+    cnt++;
+}
+/*
+세로
     ctx.fillStyle="#000";
     ctx.font="20px Arial";
     ctx.fillText(name,50+100*cnt,680);
@@ -35,4 +59,16 @@ function draw(name,tall){
         ctx.fillRect(50+100*cnt,(640-(20*i)), 50,20);
     }
     cnt++;
-}
+
+가로
+ ctx.fillStyle="#000";
+    ctx.font="20px Arial";
+    ctx.fillText(name,50,50+40*cnt);
+
+    for(var i=0; i<tall/10 ; i++){
+        ctx.fillStyle=color[cnt];
+        ctx.fillRect(150+(20*i),31+40*cnt, 20,20);
+    }
+    cnt++;
+
+*/
