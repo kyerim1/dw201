73f369d31efd564b5b47820c73d194bb745f03b6
kyerim1 <nilzero25@gmail.com>
2023-08-28 17:29:00 +0900

    0828

---
+++ 8월/0828study/static/js/main.js
@@ -0,0 +1,6 @@
+import sum from './mymodule.js';
+
+
+window.onload=function(){
+   console.log(sum(10,2));
+}
---
+++ 8월/0828study/static/js/mdtest.js
@@ -0,0 +1,10 @@
+function output(){
+    console.log("출력 테스트");
+}
+
+
+function output2(){
+
+    console.log(num);
+}
+output2();
