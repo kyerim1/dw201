0b80876ccdd8d1e749b3b40ff90d769a07252fab
kyerim1 <nilzero25@gmail.com>
2023-08-29 17:29:23 +0900

    nodejs

--- NodeJS/web-nodejs2/App.js
+++ NodeJS/web-nodejs2/App.js
@@ -3,11 +3,17 @@
 var http= require('http');
 var fs = require('fs');
 var tempUrl=require('url');
+var cookie =require('cookie');
+
+const data = JSON.parse(fs.readFileSync('./data/member.json','utf8'));
+//console.log(data);
 
 var app = http.createServer(function(request,response){
     var url = request.url;
     var query = tempUrl.parse(url,true).query;
-    console.log(query.part);
+    //console.log(query.part);
+
+
     if(query.part == undefined){
         if(request.url=='/')
             url='/src/index.html';
@@ -17,14 +23,40 @@ var app = http.createServer(function(request,response){
             url='/src/question.html';
         if(request.url=='/login')
             url='/src/login.html';
-    }else{
 
+        response.writeHead(200);
+    }else{
+        var page = query.part;
+        var isLogin='false';
+        var id='';
+        if(page==='login_check'){
+            for(var i in data){
+                if( data[i].sdmId === query.sdmId && data[i].sdmPw===query.sdmPw){
+                    isLogin='true';//아이디비번 일치하면 쿠키 생성
+                    id=query.sdmId;
+                    break;
+                }
+            }
+            url='/src/'+page+'.html';
+        }
+        response.writeHead(200,{
+            'Set-Cookie':['isLogin='+isLogin, 'id='+id]
+        });
     }
     if(request.url =='/favicon.ico'){
         return response.writeHead(404);
     }
-    response.writeHead(200);
+
+    //console.log(request.headers.cookie); 
+    //var cookies ={};
+    //cookies = cookie.parse(request.headers.cookie);// 내PC에 저장된 쿠키가져와 객체로저장
+    //console.log(cookies.id);
+    
     response.end(fs.readFileSync(__dirname+url));
+    // response.writeHead(200,{
+    //     'Set-Cookie':['id=sky','pw=1234']
+    // });
+    // response.end('aa');
 });
 app.listen(3000);
 
