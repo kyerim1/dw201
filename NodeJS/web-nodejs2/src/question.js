439b7d3c73e4eb7c051c16c20d886d3c6320960f
kyerim1 <nilzero25@gmail.com>
2023-08-30 15:56:23 +0900

    0830

--- NodeJS/web-nodejs2/src/question.js
+++ NodeJS/web-nodejs2/src/question.js
@@ -12,4 +12,32 @@ $(function(){
         login='<div class="login_bt"><a href="/login">로그인</a></div>';
     }
     $("#side").append(login);
+
+    $.getJSON("./data/question.json",function(qdata){
+        qdata.sort(function(a,b){
+            if(a.date < b.date ) return 1;
+            if(a.date > b.date ) return -1;
+            return 0;
+        });
+        $.each(qdata,function(i,q){
+            var ans = q.to==0?'<i class="bi bi-question-square"></i>':'<i class="bi bi-r-square-fill"></i>';
+            $("#qs").append('<tr><td class="num">'+(i+1)+'</td>'+
+            '<td class="title">'+q.title+'</td><td class="writer">'+q.writer+'</td>'+
+            '<td class="date">'+q.date+'</td><td class="ans">'+ans+'</td></tr>');
+        });
+    });
+
+    $("#word").on("keyup",function(){
+        var word= $(this).val();
+        $("#qs>tr").filter(function(){
+            var isShow=true;
+            if( !($(this).find(".title").text().indexOf(word) > -1  ||
+                        $(this).find(".writer").text().indexOf(word) > -1 ) )
+                isShow=false;
+            $(this).toggle( isShow);
+        });
+    });
 });
+function questionWrite(){
+    
+}
