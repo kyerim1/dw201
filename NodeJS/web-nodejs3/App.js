c491f24af506d81af1e02bee0038faf63c671398
kyerim1 <nilzero25@gmail.com>
2023-08-31 15:32:33 +0900

    0831

--- NodeJS/web-nodejs3/App.js
+++ NodeJS/web-nodejs3/App.js
@@ -9,14 +9,47 @@ const app = http.createServer(function(request, response){
     const path = url.parse(pageURL,true).pathname; //루트도메인 뒤 주소
 
     if( path ==="/"){
-        data=fs.readFileSync('./lib/page.json', 'utf8');
+        fs.readFile('./lib/page.json', 'utf8',function(err,data){
             const dataParse = JSON.parse(data);
-            const html = template.homeHTML(dataParse.main);
-            console.log(html);
+            const html = template.homeHTML(dataParse.main, dataParse.login_before);
             response.writeHead(200);
             response.write(html);
             response.end();
-        
+        });
     }
+    if(path==='/login'){
+        fs.readFile('./lib/page.json', 'utf8',function(err,data){
+            const dataParse = JSON.parse(data);
+            const html = template.loginHTML(dataParse.main);
+            response.writeHead(200);
+            response.write(html);
+            response.end();
+        });
+    }
+    if(path.indexOf('.css') > -1){
+        var css_name = path.slice('/lib/'.length);
+        fs.readFile('./lib/'+css_name,function(err,data){
+            response.writeHead(200);
+            response.write(data);
+            response.end();
+        });
+    }
+    if(path.indexOf('/image') > -1){
+        var img_name = path.slice('/image/'.length);
+        fs.readFile('./image/'+img_name,function(err,data){
+            response.writeHead(200);
+            response.write(data);
+            response.end();
+        });
+    }
+
 });
 app.listen(3000);
+
+
+
+
+/*
+    로그인 버튼 클릭시 로그인 화면이 나오게 만드세요 !
+
+*/
