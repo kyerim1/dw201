d1ecf5b08d5be0d6275ab42147e9487b8f240f7b
kyerim1 <nilzero25@gmail.com>
2023-08-31 15:30:49 +0900

    0831

--- NodeJS/web-nodejs3/lib/template.js
+++ NodeJS/web-nodejs3/lib/template.js
@@ -1,43 +1,82 @@
 // template.js
 
 module.exports = {
-    homeHTML:function(main){
-        var menu = "";
-        for(var key of Object.keys(main.menu)){
-            menu+=`<li class="menu"><a href="/${key}">${main.menu[key]}</a></li>`;
-        }
-
-        return (`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>스드메의 모든것</title>
-
-
-</head>
-<body>
-    <div id="wrap">
-        <header id="header">
-            <div class="logo"><a href="/"><img src="./image/${main.logo}"></a></div>
-            <nav class="nav">
-                <ul class="menuList">
-                   ${menu}
-                </ul>
-            </nav>
-        </header>
-        <main id="main">
-            <section id="content">
-                <img src="./image/${main.main_img}">
+    homeHTML:function(main, login){
+        var mainHTML=`
+        <section id="content">
+            <img src="./image/${main.main_img}">
+        </section>
+        <section id="side">
+            <div class="login_bt">
+                <a href="/${login.url}">${login.text}</a>
+            </div>
+        </section>`;
+        return ( commonHTML(main,mainHTML,"index") );
+    },
+    
+    loginHTML:function(main){
+        var loginHTML=`
+        <section id="content">
+                <h3>스드메 with Seonhyang 로그인</h3>
+               <div class="login_wrap">
+                    <form name="loginf" id="loginf" method="get" action="/">
+                        <input type="hidden" name="part" value="login_check">
+                        <div class="login_box">
+                                <div class="login_input">
+                                    <input type="text" name="sdmId" id="sdmId">
+                                    <label for="sdmId">ID</label>
+                                </div>
+                                <div class="login_input">
+                                    <input type="password" name="sdmPw" id="sdmPw">
+                                    <label for="sdmPw">PW</label>
+                                </div>
+                                <div class="login_bt">
+                                    <a href="javascript:login();">LOGIN</a>
+                                </div>
+                        </div>
+                    </form>
+                    <div class="joinAfind">
+                        <p class="find">아이디/비밀번호 찾기</p>
+                        <p class="join"><a href="/sign">회원가입</a></p>
+                    </div>
+               </div>
             </section>
-            <section id="side">
-                
-            </section>
-        </main>
-    </div>
-</body>
-</html>
-        `);
+        `;
+        return (commonHTML(main,loginHTML,"login"));
+    },
+}
+
+function commonHTML(main, html,css){
+    var menu = "";
+    for(var key of Object.keys(main.menu)){
+        menu+=`<li class="menu"><a href="/${key}">${main.menu[key]}</a></li>`;
     }
+    return (`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>스드메의 모든것</title>
+    
+        <link rel="stylesheet" href="./lib/main.css">
+        <link rel="stylesheet" href="./lib/${css}.css">
+    </head>
+    <body>
+        <div id="wrap">
+            <header id="header">
+                <div class="logo"><a href="/"><img src="./image/${main.logo}"></a></div>
+                <nav class="nav">
+                    <ul class="menuList">
+                       ${menu}
+                    </ul>
+                </nav>
+            </header>
+            <main id="main">
+                ${html}
+            </main>
+        </div>
+    </body>
+    </html>        
+    `);
 }
