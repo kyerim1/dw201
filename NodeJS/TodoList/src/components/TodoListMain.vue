f55d5f5bcd5cd0687e5f54fe6634797d880fd2d7
kyerim1 <nilzero25@gmail.com>
2023-09-06 15:45:47 +0900

    0906

--- NodeJS/TodoList/src/components/TodoListMain.vue
+++ NodeJS/TodoList/src/components/TodoListMain.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="mb-3">
-        <div >
-            <em></em>
+    <TodoListMenu class="p-0"/>
+    <div v-for="key in Object.keys(filter_todos)" :key="key" class="mb-3">
+        <div v-if="use_category">
+            <em>{{ key }}</em>
         </div>
+        <TodoList :data="filter_todos[key]" />
     </div>
     <div class="my-2 mt-5">
         <span style="background-color: red;">&nbsp;</span>&nbsp;
         <strong>처리하지 못한 할일</strong>
     </div>
-
+    <TodoList :data="pending_todos" />
 </template>
 
 <script>
+import TodoListMenu from '../components/TodoListMenu.vue'
+import TodoList from '../components/TodoList.vue'
 </script>
 
 <style>
--- NodeJS/TodoList/src/views/TodoListContainer.vue
+++ NodeJS/TodoList/src/views/TodoListContainer.vue
@@ -16,7 +16,41 @@
         name : "TodoListContainer",
         components:{
             TodoListNew,TodoListMain
-        }
+        },
+        setup(){
+            const todos = ref([])
+            const {loadTodos, saveTodos, storage_id} = useStorage()
+            provide('todos',readonly(todos) )
+
+            const initTodos = (init_todos) =>{
+                todos.value = init_todos
+            }
+            const addTodo = (job, date)=>{  //새로운 할일 등록
+                todos.value.push( {
+                    id: storage_id.value++,
+                    job: job,
+                    date: date,
+                    completed : false,
+                })
+                saveTodos(todos)
+            }
+            const removeTodo = (id) => {  // 할일  삭제
+                todos.value.splice(id,1)
+                todos.value.forEach( (todo,idx) => {
+                    todo.id=idx
+                })
+                saveTodos(todos)
+            }
+            const completeTodo = (id) =>{// 할일 완료 했으면 완료 처리
+                todos.value.find( (todo) => todo.id == id).completed=true
+                saveTodos(todos)
+            }
+            provide('addTodo',addTodo)
+            provide('removeTodo',removeTodo)
+            provide('completeTodo',completeTodo)
+
+            loadTodos(initTodos) // 할일 목록 불러오기
+        },
     }
 </script>
 
