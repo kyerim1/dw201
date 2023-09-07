cdca61d021ee30f7c3cc34cca2e0106828585f49
kyerim1 <nilzero25@gmail.com>
2023-09-07 18:00:21 +0900

    0907

---
+++ NodeJS/ILoveSchool/src/main.js
@@ -0,0 +1,34 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import firebase from 'firebase'
+import VueSession from 'vue-session/index'
+
+var sessionOptions={
+  persist:true
+}
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWHOW-US4jKBcLLtczKEFyZ2SBg6BZzPc",
+    authDomain: "iloveschool-f6efc.firebaseapp.com",
+    projectId: "iloveschool-f6efc",
+    storageBucket: "iloveschool-f6efc.appspot.com",
+    messagingSenderId: "70645106048",
+    appId: "1:70645106048:web:078502189019f67bf0ad65",
+    measurementId: "G-4TRGP0LV75"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+const app = createApp(App)
+
+app.use(VueSession, sessionOptions)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ NodeJS/ILoveSchool/src/views/CelebrationMsg.vue
@@ -0,0 +1,18 @@
+<!--  CelebrationMsg.vue  -  로그인 하지 않으면 못들어와! -->
+
+<template>
+    <div class="MsgBoard">
+
+        축하메시지 목록과 작성과 수정과 삭제 가 되는 페이지입니다.
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'CelebrationMsg',
+    }
+</script>
+
+<style>
+
+</style>
